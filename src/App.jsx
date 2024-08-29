86a397c87d9a5c4e84c6a820d675798d6326e90f
Trongtinnn <huynhtrongtin546@gmail.com>
2024-08-29 15:58:29 +0700

    video 27 arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
+  const App = () => {
+    const [count, setCount] = useState(0)
+
+
+  // function myFunction() {
+  //   console.log("adadaddad  ")
+  // }
+  //
+  const myFunction = () => {
+    console.log("addadadada")
+  }
+  myFunction();
 
   return (
     <>
