6211923287bc3bba60a1a776a3fc4efa757e8d68
Trongtinnn <huynhtrongtin546@gmail.com>
2024-09-06 00:03:15 +0700

    video 33- component

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,21 @@
 import './style.css'
     
 const MyComponent = () => {
+    // const tin = 25;  number
+    // const tin = "hoc"; string 
+    // const tin = true; boolean
+    // const tin = undefined; 
+    // const tin = null;
+    // const tin = {
+    //     name : "tin",
+    //     tuoi : 22
+    // };
+    const tin = [1,2,3]
+
     return (
         <>
-            <h2>Hello asa</h2>
+            <h2>{JSON.stringify(tin)} Hello asa</h2>
+            <h2>{console.log("adadad")}</h2>
             <h2 className="child" style={
                 {color: "red"}
             }>Hello 2</h2>
